0dc971391a25034b709f8616f9b22e2b71b85264
Rafael Nunes Rios <rafaelrafssk@hotmail.com>
2020-10-21 13:01:42 -0300

    third lecture

---
+++ Instagram/Instagram/Resources/Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Extensions.swift
+//  Instagram
+//
+//  Created by Rafael Nunes Rios on 10/21/20.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+}
